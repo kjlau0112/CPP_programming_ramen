e3d32ccaf9d47e477654f6c2cba4a991b1646b01
kjlau0112 <lkarnjye@gmail.com>
2024-11-18 22:02:15 +0800

    Added model manipulation via controller
    Signed-off-by: kjlau0112 <lkarnjye@gmail.com>

--- mvc/mvc.cpp
+++ mvc/mvc.cpp
@@ -94,7 +94,9 @@ int main() {
     DefectView defectView;
 
     DefectController defectController(defectModel, defectView);
-                    
+
+    defectController.addDefect("WTF", "Unexpected behavior observed.");  // Add new defect
+    defectController.showDefectComponent("WTF");                         
     std::cout << '\n';
     defectController.showDefectSummary();                                
     std::cout << '\n';
