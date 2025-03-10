bb33a7d778bcd79e78ff08c45b19bb5fc5db2338
kjlau0112 <lkarnjye@gmail.com>
2025-03-10 21:56:48 +0800

    MVC initial commit
    Stolen:
    https://www.modernescpp.com/index.php/model-view-controller/#:~:text=The%20MVC%20divides%20the%20program,controller%20interacts%20with%20the%20user
    
    Signed-off-by: kjlau0112 <lkarnjye@gmail.com>

---
+++ mvc/mvc.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class DefectModel {
+ public:
+    mutable std::unordered_map<std::string, std::string> defects_ = { {"XYZ" , "File doesn't get deleted."},
+                                                                      {"XAB" , "Registry doesn't get created."},
+                                                                      {"ABC" , "Wrong title get displayed."} };
+
+    std::string getDefectComponent(const std::string& component) const {
+        return defects_[component];
+    }
+
+    int getSummary() const {
+        return defects_.size();
+    }
+
+    std::unordered_map<std::string, std::string> getAllDefects() const {
+        return defects_;
+    }
+
+    // New method to add defects
+    void addDefect(const std::string& name, const std::string& description) {
+        defects_[name] = description;
+    }
+};
+
+class DefectView {
+ public:
+    void showSummary(int num) const {
+        std::cout << "There are " + std::to_string(num) + " defects in total!\n";
+    }
+    
+    void showDefectComponent(const std::string& defect) const {
+        std::cout << "Defect of component: " + defect + '\n';
+    }
+
+    void showDefectList(const std::unordered_map<std::string, std::string>& defects) const {
+        for (const auto& p: defects) {
+            std::cout << "(" + p.first + ", " + p.second + ")\n";
+        }
+    }
+};
+
+class DefectController {
+    DefectModel& defectModelRef;
+    const DefectView& defectViewRef;
+ public:
+    DefectController(DefectModel& defModelParameter, const DefectView& defViewParameter):
+                     defectModelRef{defModelParameter}, defectViewRef{defViewParameter} { }
+
+    void showDefectComponent(const std::string& component) const {
+        defectViewRef.showDefectComponent(defectModelRef.getDefectComponent(component));
+    }
+
+    void showDefectSummary() const {
+        defectViewRef.showSummary(defectModelRef.getSummary());
+    }
+
+    void showDefectList() const {
+        defectViewRef.showDefectList(defectModelRef.getAllDefects());
+    }
+
+    // New method to add a defect
+    void addDefect(const std::string& name, const std::string& description) {
+        defectModelRef.addDefect(name, description);
+    }    
+};
+
+int main() {
+
+    std::cout << '\n';
+
+    DefectModel defectModelInstance;
+    DefectView defectViewInstance;
+
+    DefectController defectController(defectModelInstance, defectViewInstance);
+          
+    std::cout << '\n';
+    defectController.showDefectSummary();                                
+    std::cout << '\n';
+    defectController.showDefectList();                                   
+
+    std::cout << '\n';
+
+    return 0;
+}
